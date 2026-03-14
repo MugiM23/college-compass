69a777f5791b03055c2087be4b42095c3bbd01de
Mugesh Kumar <mugeshkumar@bookmyservice.io>
2026-03-14 18:35:04 +0530

    Fixed build failures

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/college-compass/' : '/',
+  base: '/',
 }));
