292cfdc98be17d808eb79c687bdb4351e999caa9
Mugesh Kumar <mugeshkumar@bookmyservice.io>
2026-03-19 10:25:13 +0530

    updated build issue fix for assets

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,41 @@ export default defineConfig(({ mode }) => ({
     },
   },
   base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui': [
+            '@radix-ui/react-accordion',
+            '@radix-ui/react-alert-dialog',
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-dropdown-menu',
+            '@radix-ui/react-select',
+            '@radix-ui/react-tabs',
+          ],
+          'utils': ['@tanstack/react-query', 'react-helmet-async'],
+        },
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|gif|svg|webp|ico|woff|woff2|eot|ttf|otf/.test(ext)) {
+            return `assets/[name]-[hash][extname]`;
+          } else if (ext === 'css') {
+            return `css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+      },
+    },
+    commonjsOptions: {
+      include: /node_modules/,
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 }));
